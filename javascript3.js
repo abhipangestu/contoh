61212146a8ef1757393a0df63215acc6234ba34f
abhipangestu <80031436+abhipangestu@users.noreply.github.com>
2021-03-04 16:16:02 +0700

    commit ketiga , looping

---
+++ javascript3.js
@@ -0,0 +1,18 @@
+let status = true;
+while ( status = true ){  
+  alert('program memasukan angka positif dan negatif')
+  let angka = parseInt(prompt('masukan angka'))
+
+  if (angka >= 0) {
+    alert('angka ini positif');
+  } else if (angka == 0) {
+    alert('angka ini adalah 0');
+  } else if (angka < 0) {
+    alert('angka ini adalah negatif');
+  } else {
+    alert('ini bukan angka');
+  }
+
+  tanya = confirm('apakah anda mau memasukan ulang?');
+  
+}
